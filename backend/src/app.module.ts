7169ea69c86cd48a62e125e230c4f68ace5c141a
VitorLima1998 <vitor.xlr8@gmail.com>
2024-08-01 16:05:51 -0400

    refactor: Update app.module.ts to import the necessary modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductModule, SharedModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
